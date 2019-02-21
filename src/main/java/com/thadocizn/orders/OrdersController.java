deae96ec1a4b7305e4f37d9f590a91cd36a03391
ThaDocIzN <rodan72@msn.com>
2019-02-21 05:40:18 -0600

    added order controller

--- src/main/java/com/thadocizn/orders/OrdersController.java
+++ src/main/java/com/thadocizn/orders/OrdersController.java
@@ -26,29 +26,28 @@ public class OrdersController {
     @Autowired
     OrderRepository orderRepo;
 
-    //customers endpoints
     @GetMapping("/customers")
-    public List<Customer> getAllCustomers(){
+    public List<Customer> getAllCustomers() {
         return customerRepo.findAll();
     }
 
     @GetMapping("/customer/order")
-    public List<Object[]> getAllCustomerOrders(){
+    public List<Object[]> getAllCustomerOrders() {
         return customerRepo.allCustomerOrders();
     }
 
     @GetMapping("/customer/name/{custname}")
-    public List<Object[]> getCustomerOrders(@PathVariable String custname){
+    public List<Object[]> getCustomerOrders(@PathVariable String custname) {
         return customerRepo.customerOrders(custname);
     }
 
     @GetMapping("/customer/order/{custcode}")
-    public List<Object[]> getCustomerOrdersByCode(@PathVariable long custcode){
+    public List<Object[]> getCustomerOrdersByCode(@PathVariable long custcode) {
         return customerRepo.customerOrdersByCode(custcode);
     }
 
     @GetMapping("/customers/custcode/{custcode}")
-    public Customer getCustomer(@PathVariable long custcode){
+    public Customer getCustomer(@PathVariable long custcode) {
         return customerRepo.findById(custcode).orElseThrow();
     }
 
@@ -58,9 +57,9 @@ public class OrdersController {
     }
 
     @PutMapping("/customer/custcode/{custcode}")
-    public Customer updateCustomer(@RequestBody Customer newCustomer, @PathVariable long custcode) throws URISyntaxException{
+    public Customer updateCustomer(@RequestBody Customer newCustomer, @PathVariable long custcode) throws URISyntaxException {
         Optional<Customer> customerToUpdate = customerRepo.findById(custcode);
-        if(customerToUpdate.isPresent()){
+        if (customerToUpdate.isPresent()) {
             newCustomer.setCustCode(custcode);
             customerRepo.save(newCustomer);
             return newCustomer;
@@ -69,23 +68,22 @@ public class OrdersController {
     }
 
     @DeleteMapping("/customers/custcode/{custcode}")
-    public Customer deleteCustomer(@PathVariable long custcode){
+    public Customer deleteCustomer(@PathVariable long custcode) {
         var foundCustomer = customerRepo.findById(custcode);
-        if(foundCustomer.isPresent()){
+        if (foundCustomer.isPresent()) {
             customerRepo.deleteById(custcode);
             return foundCustomer.get();
         }
         return null;
     }
 
-    //orders endpoints
     @GetMapping("/orders")
-    public List<Order> getAllOrders(){
+    public List<Order> getAllOrders() {
         return orderRepo.findAll();
     }
 
     @GetMapping("/orders/ordnum/{ordnum}")
-    public Order getOrder(@PathVariable long ordnum){
+    public Order getOrder(@PathVariable long ordnum) {
         return orderRepo.findById(ordnum).orElseThrow();
     }
 
@@ -95,9 +93,9 @@ public class OrdersController {
     }
 
     @PutMapping("/orders/ordnum/{ordnum}")
-    public Order updateOrder(@RequestBody Order newOrder, @PathVariable long ordnum) throws URISyntaxException{
+    public Order updateOrder(@RequestBody Order newOrder, @PathVariable long ordnum) throws URISyntaxException {
         Optional<Order> orderToUpdate = orderRepo.findById(ordnum);
-        if(orderToUpdate.isPresent()){
+        if (orderToUpdate.isPresent()) {
             newOrder.setOrdNum(ordnum);
             orderRepo.save(newOrder);
             return newOrder;
@@ -106,34 +104,33 @@ public class OrdersController {
     }
 
     @DeleteMapping("/orders/ordnum/{ordnum}")
-    public Order deleteOrder(@PathVariable long ordnum){
+    public Order deleteOrder(@PathVariable long ordnum) {
         var foundIt = orderRepo.findById(ordnum);
 
-        if (foundIt.isPresent()){
+        if (foundIt.isPresent()) {
             orderRepo.deleteById(ordnum);
             return foundIt.get();
         }
         return null;
     }
 
-    //agents endpoints
     @GetMapping("/agents")
-    public List<Agent> getAllAgents(){
+    public List<Agent> getAllAgents() {
         return agentRepo.findAll();
     }
 
     @GetMapping("/agents/agentcode/{agentcode}")
-    public Agent getAgent(@PathVariable long agentcode){
+    public Agent getAgent(@PathVariable long agentcode) {
         return agentRepo.findById(agentcode).orElseThrow();
     }
 
     @GetMapping("/agents/customers")
-    public List<Object[]> getAgentsAndCustomers(){
+    public List<Object[]> getAgentsAndCustomers() {
         return agentRepo.agentsAndCustomers();
     }
 
     @GetMapping("/agents/orders")
-    public List<Object[]> getAllAgentOrders(){
+    public List<Object[]> getAllAgentOrders() {
         return agentRepo.allAgentOrders();
     }
 
@@ -143,9 +140,9 @@ public class OrdersController {
     }
 
     @PutMapping("/agents/agentcode/{agentcode}")
-    public Agent updateAgent(@RequestBody Agent newAgent, @PathVariable long agentcode) throws URISyntaxException{
+    public Agent updateAgent(@RequestBody Agent newAgent, @PathVariable long agentcode) throws URISyntaxException {
         Optional<Agent> agentToUpdate = agentRepo.findById(agentcode);
-        if(agentToUpdate.isPresent()){
+        if (agentToUpdate.isPresent()) {
             newAgent.setAgentCode(agentcode);
             agentRepo.save(newAgent);
             return newAgent;
@@ -156,7 +153,7 @@ public class OrdersController {
     @DeleteMapping("/agents/agentcode/{agentcode}")
     public Agent deleteAgent(@PathVariable long agentcode) {
         var foundAgent = agentRepo.findById(agentcode);
-        if(foundAgent.isPresent()) {
+        if (foundAgent.isPresent()) {
             agentRepo.deleteById(agentcode);
             return foundAgent.get();
         }
